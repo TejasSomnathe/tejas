d378ca836b349a69188537627664d91b6d2121a4
TejasSomnathe <tejassomnathe@gmail.com>
2025-02-25 16:15:48 +0000

    making footer

--- Portfolio/src/components/components2/Footer.jsx
+++ Portfolio/src/components/components2/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-
+import RollingGallery from './RollingGallery'
+  
 function Footer() {
   return (
     <div>
         
+      <RollingGallery autoplay={true} pauseOnHover={true} />    
         
     </div>
   )
